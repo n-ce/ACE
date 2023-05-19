6e03f6cdf17e5601d1007096d4b0af450d11b3bc
n-ce <69345507+n-ce@users.noreply.github.com>
2023-05-19 11:59:34 +0000

    For now

--- api/index.js
+++ api/index.js
@@ -1,35 +1,12 @@
-const querystring = require('querystring');
-const express = require('express');
+import querystring from 'querystring';
+import express from 'express';
 const app = express();
+import { getAverageColor } from 'fast-average-color-node';
 
 app.get('/api/*', (req, res) => { 
-  fetch(req.params[0] + "?" + querystring.stringify(req.query))
-    .then(res => res.blob())
-		.then(blob => blob.arrayBuffer())
-		.then(arrayBuffer => Buffer.from(arrayBuffer))
-    .then(buffer => Uint8ClampedArray.from(buffer))
-		.then(data => {
-			const len = data.length;
-      const nthPixel = 40;
-      let r = 0;
-      let g = 0;
-      let b = 0;
-      for (let i = 0; i < len; i += nthPixel) {
-        r += data[i];
-        g += data[i + 1];
-        b += data[i + 2];
-      }
-      const amount = len / nthPixel;
-      r /= amount;
-      g /= amount;
-      b /= amount;
-		
-			res.send(data.length);
-      // res.send(`${Math.floor(r)},${Math.floor(g)},${Math.floor(b)}`);
-    })
-    .catch(err => {
-      res.send(`error: ${err.message}`);
-    });
+	getAverageColor(`https://${req.params[0]}?${querystring.stringify(req.query)}`)
+	.then(color=>res.send(color))
+	.catch(err=>res.send(err))
 });
 
 app.listen(3000, () => {
